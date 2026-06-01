931bf98fb8a80f047a63a965f96674f5ac13f0e9
TEJAS <tejasdeshmane66@gmail.com>
2026-06-01 19:58:49 +0530

    chore: configure tooling (eslint, prettier, vitest, husky, commitlint)

---
+++ commitlint.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'ci'],
+    ],
+    'subject-max-length': [2, 'always', 72],
+  },
+};
+
+export default config;
